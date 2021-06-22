04d6bb92bc32fedeacb5ca92cbe1afb18f73c4a6
Shreyas Mayya <smayya337@gmail.com>
2021-06-22 11:41:58 -0400

    track -> track_name

--- lib/sciolyff/interpreter/track.rb
+++ lib/sciolyff/interpreter/track.rb
@@ -7,9 +7,9 @@ module SciolyFF
   class Interpreter::Track < Interpreter::Model
     def link_to_other_models(interpreter)
       super
-      @teams = interpreter.teams.find { |t| t.track == @rep[:name] }
-      @placings = interpreter.placings.find { |p| p.team.track == @rep[:name] }
-      @penalties = interpreter.penalties.find { |p| p.team.track == @rep[:name] }
+      @teams = interpreter.teams.find { |t| t.track_name == @rep[:name] }
+      @placings = interpreter.placings.find { |p| p.team.track_name == @rep[:name] }
+      @penalties = interpreter.penalties.find { |p| p.team.track_name == @rep[:name] }
     end
 
     attr_reader :teams, :placings, :penalties
